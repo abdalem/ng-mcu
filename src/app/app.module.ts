d9e916195e2dc1eba12c900c76352728e0fb513f
abdalem <abdennour@abbi-studio.fr>
2019-09-25 14:20:45 +0000

    dev: set shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
 import { AppRoutingModule } from '@app/configs';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
